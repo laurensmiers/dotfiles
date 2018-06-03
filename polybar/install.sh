ec8dcfd07a8c18b56a1a61a2625fa2701da106d6
laurensmiers <miers132@gmail.com>
2018-06-04 01:16:19 +0200

    Polybar: add initial config

---
+++ polybar/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# TODO: assumption here, git checkout in ~/dotfiles
+
+# make stumpwm directory
+echo "Make polybar directory..."
+mkdir -p ~/.config/polybar || true
+
+# Link to git dot-file
+echo "Link to our dotfile..."
+ln -s ~/dotfiles/polybar/config ~/.config//polybar/config
+ln -s ~/dotfiles/polybar/launch.sh ~/.config//polybar/launch.sh
+
+echo "DONE....."
