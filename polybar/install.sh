cd9d000198489e78bd5a5f42b35d19f13d531699
laurensmiers <miers132@gmail.com>
2018-06-04 23:19:26 +0200

    Stumpw / polybar: stumpwm + polybar ipc

--- polybar/install.sh
+++ polybar/install.sh
@@ -7,7 +7,8 @@ mkdir -p ~/.config/polybar || true
 
 # Link to git dot-file
 echo "Link to our dotfile..."
-ln -s ~/dotfiles/polybar/config ~/.config//polybar/config
-ln -s ~/dotfiles/polybar/launch.sh ~/.config//polybar/launch.sh
+ln -s ~/dotfiles/polybar/config ~/.config//polybar/config || true
+ln -s ~/dotfiles/polybar/launch.sh ~/.config//polybar/launch.sh || true
+ln -s ~/dotfiles/polybar/stumpwmgroups.sh ~/.config//polybar/stumpwmgroups.sh || true
 
 echo "DONE....."
