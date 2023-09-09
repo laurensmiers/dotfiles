f87500419dde90ec6d651e088f87056a51fa4a49
Laurens Miers <laurensmiers@protonmail.com>
2023-09-09 17:54:11 +0200

    WAYBAR: Add waybar config

--- sway/install.sh
+++ sway/install.sh
@@ -3,3 +3,7 @@
 mkdir -p ~/.config/sway
 rm  ~/.config/sway/config
 ln -s ~/projects/dotfiles/sway/config ~/.config/sway/config
+
+mkdir -p ~/.config/sway/waybar
+rm  ~/.config/waybar/config
+ln -s ~/projects/dotfiles/sway/waybar/config ~/.config/waybar/config
