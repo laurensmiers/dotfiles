cad54aa9e92b3ca230b288b3b1c6dffd7b0b4f8b
Laurens Miers <laurensmiers@protonmail.com>
2023-09-13 17:04:20 +0200

    SWAY: Add screenshot script

--- sway/install.sh
+++ sway/install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+mkdir -p ~/.local/bin
+ln -s ~/projects/dotfiles/sway/screenshot.sh ~/.local/bin/screenshot 2>/dev/null || true
+
 mkdir -p ~/.config/sway
 rm  ~/.config/sway/config
 ln -s ~/projects/dotfiles/sway/config ~/.config/sway/config
---
+++ sway/screenshot.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+grim -g "$(slurp -d)" ~/Pictures/screenshot_$(date +%Y-%m-%e_%k-%M-%S).png
